5818fdb6fc2f9e4e9ed622e5297f0b27d5b09c0f
Daniel O'Brien <114220255+Daniel-O-Brien@users.noreply.github.com>
2023-11-15 16:40:30 +0000

    Added Variables

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+public class Game {
+    private String name;
+
+    private String publisher;
+
+    private String description;
+
+    private String originalDeveloper;
+
+    private String gamesMachine;
+
+    private int yearOfRelease;
+
+    private String image;
+
+
+}
