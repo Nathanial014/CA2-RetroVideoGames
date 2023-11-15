aea88da23ab030050341b63425c90d58c8c3251f
Nathanial <20103232@mail.wit.ie>
2023-11-15 16:22:16 +0000

    Comment added

--- src/GamesMachine.java
+++ src/GamesMachine.java
@@ -2,4 +2,5 @@ public class GamesMachine {
     private String machinename, manufacturer, description, type, media;
     private int initalLaunchYear;
     System.out.println("OMG ITS WORKING");
+    // Here We Go
 }
