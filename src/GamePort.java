d5e3e38b2fd69d7d0848352bd40151ec639aa746
Nathanial <20103232@mail.wit.ie>
2023-11-22 11:02:03 +0000

    Games Port Added and Quciksort started.

--- src/GamePort.java
+++ src/GamePort.java
@@ -1,6 +1,53 @@
 public class GamePort {
     private String originalGamePort, newGameMachinePortTo, portDeveloper;
     private int releaseYearOfPort;
-    private int coverArt;
+    private String coverArt;
 
+    public GamePort(String originalGamePort, String newGameMachinePortTo, String portDeveloper, int releaseYearOfPort, String coverArt) {
+        setOriginalGamePort(originalGamePort);
+        setNewGameMachinePortTo(newGameMachinePortTo);
+        setPortDeveloper(portDeveloper);
+        setReleaseYearOfPort(releaseYearOfPort);
+        setCoverArt(coverArt);
+    }
+
+    public String getOriginalGamePort() {
+        return originalGamePort;
+    }
+
+    public void setOriginalGamePort(String originalGamePort) {
+        this.originalGamePort = originalGamePort;
+    }
+
+    public String getNewGameMachinePortTo() {
+        return newGameMachinePortTo;
+    }
+
+    public void setNewGameMachinePortTo(String newGameMachinePortTo) {
+        this.newGameMachinePortTo = newGameMachinePortTo;
+    }
+
+    public String getPortDeveloper() {
+        return portDeveloper;
+    }
+
+    public void setPortDeveloper(String portDeveloper) {
+        this.portDeveloper = portDeveloper;
+    }
+
+    public int getReleaseYearOfPort() {
+        return releaseYearOfPort;
+    }
+
+    public void setReleaseYearOfPort(int releaseYearOfPort) {
+        this.releaseYearOfPort = releaseYearOfPort;
+    }
+
+    public String getCoverArt() {
+        return coverArt;
+    }
+
+    public void setCoverArt(String coverArt) {
+        this.coverArt = coverArt;
+    }
 }
