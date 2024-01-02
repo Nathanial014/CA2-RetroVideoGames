50876f1510c1120152e0b2a4244814321fbcf9f6
Daniel O'Brien <114220255+Daniel-O-Brien@users.noreply.github.com>
2024-01-02 15:35:28 +0000

    Saving sort of works but not loading

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 import java.io.*;
+import java.security.spec.ECField;
 
 import com.thoughtworks.xstream.XStream;
 import javafx.scene.control.TextArea;
@@ -14,18 +15,13 @@ import javafx.scene.control.TextField;
 public class Menu {
 
     @FXML
-    private TextField machineName, manufacturer, type, media, initialLaunchYear, initialPrice, image, name, publisher, originalDeveloper, gamesMachine, coverArt, yearOfRelease;
+    private TextField machineName, manufacturer, type, media, initialLaunchYear, initialPrice, image, gameName, publisher, originalDeveloper, gamesMachine, coverArt, yearOfRelease;
 
     @FXML
     private TextArea gamesMachineDescription, gameDescription;
 
     @FXML
-    private ListView<String> gamesMachineView;
-
-    @FXML
-    private ListView<String> gamesView;
-
-    private File file;
+    private ListView<String> gamesMachineView, gamesView;
 
     CuckooHash gamesMachineHash = new CuckooHash<GamesMachine>(7, 15, k->k.getId()%15,k->(k.getId()*13)%15,k->((k.getId()*19)/2)%15,k->(k.getId()*17)%15,k->(k.getId()+7)%15,k->(k.getId()*k.getId()+12)%15,k->(k.getId()*3+5)%15);
 
@@ -33,10 +29,9 @@ public class Menu {
     public void initialize() {
 //        load();
 //        gamesMachineListView.getItems().addAll(file.toString());
-        this.file = new File("retroVideoGames");
     }
 
-    public void addGamesMachine() {
+    public void addGamesMachine() throws Exception{
         gamesMachineHash.add(new GamesMachine(machineName.getText(), manufacturer.getText(),gamesMachineDescription.getText(), type.getText(), media.getText(), Integer.parseInt(initialLaunchYear.getText()), Integer.parseInt(initialPrice.getText()), image.getText()));
         gamesMachineView.getItems().add(machineName.getText());
         machineName.clear();
@@ -47,31 +42,36 @@ public class Menu {
         initialPrice.clear();
         image.clear();
         gamesMachineDescription.clear();
+        save();
     }
 
-    public void addGame() {
-        gameHash.add(new Game(name.getText(), publisher.getText(),gameDescription.getText(), originalDeveloper.getText(), gamesMachine.getText(), Integer.parseInt(yearOfRelease.getText()), coverArt.getText()));
-        gamesView.getItems().add(name.getText());
-        name.clear();
+    public void addGame() throws Exception {
+        gameHash.add(new Game(gameName.getText(), publisher.getText(),gameDescription.getText(), originalDeveloper.getText(), gamesMachine.getText(), Integer.parseInt(yearOfRelease.getText()), coverArt.getText()));
+        gamesView.getItems().add(gameName.getText());
+        gameName.clear();
         publisher.clear();
         gameDescription.clear();
         originalDeveloper.clear();
         gamesMachine.clear();
         yearOfRelease.clear();
         coverArt.clear();
+        save();
     }
 
     public void save() throws Exception {
         XStream xstream = new XStream(new DomDriver());
-        ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter(file));
-        out.writeObject(gamesMachine);
-        out.close();
+        ObjectOutputStream gamesMachineOut = xstream.createObjectOutputStream(new FileWriter("gamesMachine"));
+        ObjectOutputStream gamesOut = xstream.createObjectOutputStream(new FileWriter("games"));
+        gamesMachineOut.writeObject(gamesMachineHash);
+        gamesOut.writeObject(gameHash);
+        gamesMachineOut.close();
+        gamesOut.close();
     }
 
     @SuppressWarnings("unchecked")
     public void load() throws Exception {
         //list of classes that you wish to include in the serialisation, separated by a comma
-        Class<?>[] classes = new Class[]{GamesMachine.class, Game.class};
+        Class<?>[] classes = new Class[]{CuckooHash.class, GamesMachine.class, Game.class};
 
         //setting up the xstream object with default security and the above classes
         XStream xstream = new XStream(new DomDriver());
@@ -79,13 +79,16 @@ public class Menu {
         xstream.allowTypes(classes);
 
         //doing the actual serialisation to an XML file
-        ObjectInputStream in = xstream.createObjectInputStream(new FileReader(file));
-        gamesMachineHash = (CuckooHash<Game>) in.readObject();
-        in.close();
+        ObjectInputStream gamesMachineIn = xstream.createObjectInputStream(new FileReader("gamesMachine"));
+        ObjectInputStream gamesIn = xstream.createObjectInputStream(new FileReader("games"));
+        gamesMachineHash = (CuckooHash<GamesMachine>) gamesMachineIn.readObject();
+        gameHash = (CuckooHash<Game>) gamesIn.readObject();
+        gamesMachineIn.close();
+        gamesIn.close();
     }
 
-    public String fileName(){
-        return this.file.toString();
-    }
+//    public String fileName(){
+//        return this.file.toString();
+//    }
 
 }
--- src/main/java/org/example/QuickSort.java
+++ src/main/java/org/example/QuickSort.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.util.Comparator;
-import java.util.List;
 
 public class QuickSort {
 //
